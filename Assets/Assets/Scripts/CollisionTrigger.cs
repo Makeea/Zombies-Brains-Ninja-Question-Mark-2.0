851737dffe0322932af023c8f050220c72edd47c
Makeea <speedynyc@gmail.com>
2019-05-30 22:59:56 -0400

    Unity Upgrade
    upgrade to Unity 2019.1.4f1 (64-bit)

---
+++ Assets/Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour
+{
+    public BoxCollider2D platformCollider;
+    public BoxCollider2D platformTrigger;
+    private BoxCollider2D playerCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerCollider = GameObject.Find("Player").GetComponent<BoxCollider2D>();    
+        Physics2D.IgnoreCollision(platformCollider, platformTrigger, true);
+    }
+        
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Physics2D.IgnoreCollision(platformCollider, playerCollider, true);
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Physics2D.IgnoreCollision(platformCollider, playerCollider, false);
+        }
+
+    }
+}
---
+++ Assets/Assets/Scripts/Knife.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D myRigidbody;
+
+    private Vector2 direction;
+
+    // Use this for initialization
+    void Start()
+    {
+      myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.velocity = direction * speed;
+        //direction = Vector2.right;
+
+    }
+
+    public void Initialize(Vector2 direction)
+    {
+        this.direction = direction;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
